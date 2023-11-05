70b6280052cd04b4e76029ab65490affeb82eff6
salaat Daar <adenali362@gmail.com>
2023-11-04 23:33:07 -0500

    add logs to trest MR

--- src/main/java/com/example/ticket/config/WebConfig.java
+++ src/main/java/com/example/ticket/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("settinh cors");
         registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000","http://localhost:8080","http://localhost","*")
                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
